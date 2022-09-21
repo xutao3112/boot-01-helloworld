68cb84f2a53c55e821c9ebd03ac02b6469a76fbd
xutao <xutao3112@gimal.com>
2022-09-21 15:59:45 +0800

    second commit

--- src/main/java/com/example/boot01helloworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/boot01helloworld/service/Impl/UserServiceImpl.java
@@ -6,13 +6,15 @@ import com.example.boot01helloworld.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 /**
  * @author xt
  * @create 2022-09-01-21:03
  **/
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
+    @Resource
     UserMapper userMapper;
     @Override
     public User getUserById(String name) {
